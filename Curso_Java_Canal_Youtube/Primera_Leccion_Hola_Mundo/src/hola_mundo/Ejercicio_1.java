fb35b49c627e5f50eeeceeb316db64d187dc3306
Adri-Dev-Coding <adricoding647@gmail.com>
2025-05-08 01:11:57 +0200

    Update Ejercicio_1.java

--- Curso_Java_Canal_Youtube/Primera_Leccion_Hola_Mundo/src/hola_mundo/Ejercicio_1.java
+++ Curso_Java_Canal_Youtube/Primera_Leccion_Hola_Mundo/src/hola_mundo/Ejercicio_1.java
@@ -1,5 +1,9 @@
 package hola_mundo;
 
 public class Ejercicio_1 {
+	/*--------EJERCICIO--------*/
+	/*--- Imprime por Pantalla el siguiente texto: "Bienvenidos al Curso de Java" ---*/
+
+	
 	//Esta es la Base que requeriremos para realizar el Ejercicio.
 }
